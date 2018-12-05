2974bb8b695f6a21e9d92dfe3a69adc5c5a0e9fb
kverna099 <44757623+kverna099@users.noreply.github.com>
2018-12-05 10:57:38 -0800

    Lab2

---
+++ L201/js.js
@@ -0,0 +1,18 @@
+var ricxvi=prompt('chawert rame')
+
+
+if (ricxvi>0){
+alert("1");
+
+
+}
+else if(ricxvi<0) {
+alert("-1");
+
+
+}
+else {
+alert(0);
+
+
+}
---
+++ L204/script.js
@@ -0,0 +1,11 @@
+var sum = "";
+
+while (true) {
+
+  var value = prompt("შეიყვანეთ რიცხვი", '');
+
+  if (value=="ccvv") break; // (*)
+
+
+}
+alert("sworia");
